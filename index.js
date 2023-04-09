36b41c7316e2065e11b11678bbd17dd7daa3b954
timfel20 <timmyfelicia@gmail.com>
2023-04-09 18:24:20 +0200

    simple blog-like website about web browsers

---
+++ index.js
@@ -0,0 +1,11 @@
+var navIcon = document.querySelector('.nav-icon');
+var menu = document.querySelector('.menu');
+
+navIcon.addEventListener('click', function() {
+    menu.classList.toggle('open');
+    navIcon.classList.toggle('open');
+console.log('working')
+
+});
+
+
